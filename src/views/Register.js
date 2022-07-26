b1db6e3421cb4d4747cf9be8333f38cbc18a154b
azhar <azhar.rachman@Hotmail.com>
2022-07-26 15:10:36 +0700

    update2

--- src/views/Register.js
+++ src/views/Register.js
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 const db = firebase.firestore();
 
 const Register = () => {
-    const navigate = useNavigate;
+    const navigate = useNavigate();
 
     const [username, setUsername] = React.useState("");
     const [password, setPassword] = React.useState("");
